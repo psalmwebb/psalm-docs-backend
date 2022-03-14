14c40b5b2da15f511dc5249a6acd0fd3ace5697d
psalmwebb <psalmyweb@gmail.com>
2022-03-14 15:46:37 +0100

    psalm docs backend

---
+++ ormconfig.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+   "type": "mysql",
+   "host": "localhost",
+   "port": 3306,
+   "username": "test",
+   "password": "test",
+   "database": "test",
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "src/entity/**/*.ts"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Document } from './users/models/Document';
+import { User } from './users/models/User';
+import { UserModule } from './users/users.module';
+import { DataModule } from './ws/data.module';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import {join} from "path";
+import {config} from "dotenv";
+
+config({path:join(__dirname,"..",".env")});
+
+// import * as CONN from "../conn";
+
+// console.log(process.env);
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.db_host,
+      port: 3306,
+      username:process.env.db_user,
+      password:process.env.db_pass,
+      database: process.env.db_name,
+      entities: [User,Document],
+      synchronize: false,
+      migrationsRun:false
+  }),
+  ServeStaticModule.forRoot({
+    rootPath:join(__dirname,"client_build")
+  }),
+    UserModule,
+    DataModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule{}
